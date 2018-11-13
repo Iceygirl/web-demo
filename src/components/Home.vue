53c679296c1d31419a3c8c150063e21830d89361
Iceygirl <1427882109@qq.com>
2018-11-13 09:11:54 +0800

    首页样式修改,特别是高度

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,16 +3,21 @@
     <div class="HomeHeader">
        <head-page></head-page>
     </div>
-    <div class="HomeMain">
-      <left-nav></left-nav>
-      <div class="HomeMain"></div>
+    <div class="HomeMain clearfix">
+      <div class="leftNav fl">
+        <left-nav></left-nav>
+      </div>
+      <div class="breadNav fl">
+        <bread-crumbs></bread-crumbs>
+      </div>
     </div>
   </div>
 </template>
- 
+  
 <script>
 import Header from 'public/Header'
-import SideNav from 'public/SideNav'
+import SideNav from 'public/SideNav' 
+import BreadCrumbs from 'public/BreadCrumbs'  
 export default {
   data () {
     return {
@@ -20,16 +25,32 @@ export default {
   },
   components: {
     'head-page': Header,
-    'left-nav': SideNav
+    'left-nav': SideNav,
+    'bread-crumbs':BreadCrumbs
   }
 } 
 </script>
 
 <style lang="scss" scoped>
-.HomeHeader {
-  width:100%;
-  height:50px;
-  line-height:50px;
-  background-color:#CAF2FE;
+.Home {
+  height:100%;
+  .HomeHeader {
+    width:100%;
+    height:50px;
+    line-height:50px;
+    background-color:#CAF2FE;
+  }
+  .HomeMain {
+    height:calc(100% - 50px); 
+    .leftNav {
+      width:200px;
+      height:100%;
+    }
+    .ivu-menu {
+      width:100% !important;
+      height:100% !important;
+    }
+  }
 }
+
 </style>
--- src/components/Public/BreadCrumbs.vue
+++ src/components/Public/BreadCrumbs.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="Bread">
-    面包屑
+    
   </div>
 </template>
 
